238323d1f2ea2f5ac0cfdc3175da6b7d20b45297
Brandon Aldred <brandonaldred@gmail.com>
2021-06-03 18:17:16 -0700

    added a ton

--- js/game.js
+++ js/game.js
@@ -1,17 +1,43 @@
-const roll = () => Math.floor(Math.random() * 6 + 1);
-const content = document.getElementById('content');
-let keep = [];
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.turn = true;
+        this.rolls = 0;
+        this.score = 0;
+        this.toRoll = 6;
+        this.rolled = [];
+        this.holding = [];
+        this.qualified = false;
+        this.wins = 0;
 
-const printRoll = (numberOfDice) => {
-    let html = '';
-    for (let i = 0; i < numberOfDice; i++) {
-        html += `<p id="${i}">${roll()}</p>`;
     }
-    document.getElementById('content').innerHTML = html;
 }
 
-printRoll(2);
+//Test array
+const players = [];
+players.push(new Player('Brandon'));
 
-content.addEventListener('click', () => {
-    printRoll(19);
-})
+function roll(num) {
+    let rolled = [];
+    for (let i = 0; i < num; i++) {
+        rolled.push(Math.ceil(Math.random() * 6));
+    }
+    return rolled;
+}
+
+let i = 0
+let player = players[i];
+player.toRoll = 6;
+
+// Players Turn loop. Need to nest in a keep function
+while(player.turn) {
+    for (let i = 0; i < player.toRoll; i++) {
+        player.rolled = roll(player.toRoll);
+    }
+    player.rolls++;
+    player.rolled.forEach(die => {
+        document.getElementById('content').innerHTML = `<p>${die}</p>`;
+    });
+    player.toRoll--;
+    player.toRoll === 0 ? player.turn = false : player.turn;
+}
